42fb453424e73c4c4e22e7a9e9120c6e2636b1b1
AmririS <AmririS@users.noreply.github.com>
2018-04-10 11:57:57 +0200

    cours4 suite3

--- cours4/script.js
+++ cours4/script.js
@@ -15,9 +15,42 @@ $( document ).ready(function() {
 
  function welcome() {
     var name= getUsername();
-    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez prêt à renouveler de nouveaux défis ! </h4>');
 
 };
 // Appel la fonction
 
+  function loadChallenges() {
+  $.ajax(
+        {url : 'https://s3.eu-central-1.amazonaws.com/spintransfer/challenge.json',
+        type: 'GET',
+        dataType: 'json'} 
+  )
+  .done(function(data) {
+    showChallenge(data);
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+}
+
+function showChallenge(liste) {
+
+  var data="";
+
+  for ( var i=0; i < liste.length; i++) {
+    data+='<div class="defi">';
+
+    var challenge=liste[i];
+
+    data+='<h2>'+challenge.nom+'</h2>';
+    data+='<p>'+challenge.description+'</p>';
+    data+='<iframe src="'+challenge.youtube+'" allowfullscreen="" width="364" height="204" frameborder="0"></iframe>'
+    data+="</div>";
+  }
+  $("#defis").html(data);
+
+}
+
+loadChallenges();
 })
