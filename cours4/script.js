0129380d6b0c14f06f68719ab63f00bc151b2e46
AmririS <AmririS@users.noreply.github.com>
2018-04-23 22:21:55 +0200

    cours4 grille

--- cours4/script.js
+++ cours4/script.js
@@ -1,24 +1,19 @@
 $( document ).ready(function() {
 
-  function welcome() {
-   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
-};
-
-  welcome();
 
   function getUsername() {
-    //affiche une boite de dialogue 
     var  user = prompt("Veuillez rentrez votre nom:", "");
-    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
     return user
 }
 
  function welcome() {
     var name= getUsername();
-    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez prêt à renouveler de nouveaux défis ! </h4>');
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez prêt à découvrir de nouveaux défis ! </h4>');
 
 };
-// Appel la fonction
+
+  welcome();
+
 
   function loadChallenges() {
   $.ajax(
